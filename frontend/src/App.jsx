5a27eac50104bf04b2673e821c44dec62ca19cf7
Jonatandb <jonatandb@gmail.com>
2024-01-25 20:03:52 -0300

    Add route to update task

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,6 +7,7 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<Home />} />
+        <Route path='/tasks/:id' element={<TaskForm />} />
         <Route path='/tasks/new' element={<TaskForm />} />
       </Routes>
     </BrowserRouter>
