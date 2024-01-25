79073af85eee1f5999b75fcbd6d949a2cd24230d
Jonatandb <jonatandb@gmail.com>
2024-01-25 20:21:23 -0300

    Add delete task functionality

--- frontend/src/pages/TaskForm.jsx
+++ frontend/src/pages/TaskForm.jsx
@@ -28,39 +28,66 @@ function TaskForm() {
     }
   }
 
+  const handleDelete = async () => {
+    try {
+      await axios.delete(`http://localhost:8000/api/tasks/${params.id}`)
+      navigate('/')
+    } catch (error) {
+      console.log(error.message)
+    }
+  }
+
   useEffect(() => {
     async function getTaskById() {
       const response = await axios.get(
         `http://localhost:8000/api/tasks/${params.id}`,
       )
-      setTitle(response.data.title)
-      setDescription(response.data.description)
+      setTitle(response.data.title ?? '')
+      setDescription(response.data.description ?? '')
     }
     if (params.id) {
+      setTitle('Loading title...')
+      setDescription('Loading description...')
       getTaskById()
     }
   }, [])
 
   return (
     <div className='flex items-center justify-center h-[calc(100vh-10rem)]'>
-      <form className='bg-zinc-950 p-10' onSubmit={handleSubmit}>
-        <input
-          type='text'
-          placeholder='Write a title'
-          className='block py-2 px-3 mb-4 w-full text-black'
-          onChange={e => setTitle(e.target.value)}
-          autoFocus
-          value={title}
-        />
-        <textarea
-          placeholder='Write a description'
-          rows='3'
-          className='block py-2 px-3 mb-4 w-full text-black'
-          onChange={e => setDescription(e.target.value)}
-          value={description}
-        />
-        <button type='submit'>{params.id ? 'Update' : 'Save'}</button>
-      </form>
+      <div>
+        <form className='bg-zinc-950 p-10' onSubmit={handleSubmit}>
+          <input
+            type='text'
+            placeholder='Write a title'
+            className='block py-2 px-3 mb-4 w-full text-black'
+            onChange={e => setTitle(e.target.value)}
+            autoFocus
+            value={title}
+          />
+          <textarea
+            placeholder='Write a description'
+            rows='3'
+            className='block py-2 px-3 mb-4 w-full text-black'
+            onChange={e => setDescription(e.target.value)}
+            value={description ?? ''}
+          />
+          <button
+            type='submit'
+            className='bg-green-500 hover:bg-green-400 text-white font-bold py-2 px-4 rounded mt-5'
+          >
+            {params.id ? 'Update' : 'Save'}
+          </button>
+        </form>
+
+        {params.id && (
+          <button
+            className='bg-red-500 hover:bg-red-400 text-white font-bold py-2 px-4 rounded mt-5'
+            onClick={() => handleDelete()}
+          >
+            Delete
+          </button>
+        )}
+      </div>
     </div>
   )
 }
